06f1505d5cbfee901b0fe0bd48893f7f2ca3d8d4
bbjarvis <brettbjarvis@gmail.com>
2022-04-11 14:05:56 -0700

    add ex5

---
+++ rubyhardway/ex5.rb
@@ -0,0 +1,17 @@
+name = 'Brett B. Jarvis'
+age = 36 # not a lie in 2021
+height = 5*12+10 # inches
+weight = 225 # lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually, that's not too heavy, hahahahahahahahaha"
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this is tricky, try to get it exaactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
