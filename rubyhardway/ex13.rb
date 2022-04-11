f9df49178f595b55a4adac7e20ceead68f453cce
bbjarvis <brettbjarvis@gmail.com>
2022-04-11 15:20:34 -0700

    add ex13

---
+++ rubyhardway/ex13.rb
@@ -0,0 +1,9 @@
+=begin
+  These variables must be declared when running app:
+    ruby ex13.rb [first, second, third]
+=end
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
