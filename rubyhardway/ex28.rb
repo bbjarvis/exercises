c63ed035424424642f43a906fea6084a684f9e54
bbjarvis <brettbjarvis@gmail.com>
2022-04-12 13:09:17 -0700

    add ex28

---
+++ rubyhardway/ex28.rb
@@ -0,0 +1,42 @@
+# Take each of these logic problems and write what you think the answer will be
+# first "puts" is guess, second is test
+puts true
+puts true && true 
+puts false
+puts false && true 
+puts false
+puts 1 == 1 && 2 == 1
+puts true
+puts "test" == "test"
+puts true
+puts 1 == 1 || 2 != 1
+puts true
+puts true && 1 == 1
+puts false
+puts false && 0 != 0
+puts true
+puts true || 1 == 1
+puts false
+puts "test" == "testing"
+puts false
+puts 1 != 0 && 2 == 1
+puts true
+puts "test" != "testing"
+puts false
+puts "test" == 1
+puts true
+puts !(true && false)
+puts false
+puts !(1 == 1 && 0 != 1)
+puts false
+puts !(10 == 1 || 1000 == 1000)
+puts false
+puts !(1 != 10 || 3 == 4)
+puts true
+puts !("testing" == "testing" && "Zed" == "Cool Guy")
+puts true
+puts 1 == 1 && (!("testing" == 1 || 1 == 0))
+puts false
+puts "chunky" == "bacon" && (!(3 == 4 || 3 == 3))
+puts false
+puts 3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
