41b753b191813b0991e3fdb348714c598a008a4c
bbjarvis <brettbjarvis@gmail.com>
2022-06-17 17:03:50 -0700

    add inheritance

---
+++ OOP/inheritance.js
@@ -0,0 +1,37 @@
+class Person {
+  // moved here b/c it was identical
+  constructor(name, quirkyFact) {
+    this.name = name;
+    this.quirkyFact = quirkyFact;
+  }
+
+  // moved here b/c it was identical
+  bio() {
+    return `My name is ${this.name} and here's my quirky fact: ${this.quirkyFact}`;
+  }
+}
+
+class Student extends Person {
+  // stays in Student class since it's specific to students only
+  enroll(cohort) {
+    this.cohort = cohort;
+  }
+}
+
+class Mentor extends Person {
+  // specific to mentors
+  goOnShift() {
+    this.onShift = true;
+  }
+
+  // specific to mentors
+  goOffShift() {
+    this.onShift = false;
+  }
+}
+
+let me = new Student('brett', 'was in a band');
+console.log(me.bio())
+
+let teach = new Mentor('Gary', 'no one is named Gary anymore')
+console.log(teach.bio())
