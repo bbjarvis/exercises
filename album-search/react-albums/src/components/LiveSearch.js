ad35d206c51cf39ca4a4ca99d861ca278c89822d
bbjarvis <brettbjarvis@gmail.com>
2022-06-20 12:38:42 -0700

    add album-search assy

---
+++ album-search/react-albums/src/components/LiveSearch.js
@@ -0,0 +1,30 @@
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+
+import SearchBar from "components/SearchBar";
+import Results from "components/Results";
+
+export default function LiveSearch(props) {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const term = 'beatles'
+    const testURL = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`
+    axios.get(testURL).then(res => {
+      setResults([...res.data.results])
+    })
+  }, [term])
+
+  return (
+    <Fragment>
+      <header className="logo">
+        <img src="images/brand.png" alt="Brand" />
+      </header>
+      <main>
+        <SearchBar onSearch={term => setTerm(term)} />
+        <Results results={results} />
+      </main>
+    </Fragment>
+  );
+}
