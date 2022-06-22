58058111e9b997b1699cc61696319cbc2c7f71c4
bbjarvis <brettbjarvis@gmail.com>
2022-06-22 07:01:23 -0700

    add fnxns and printing_nested_arrays

---
+++ fnxns/printing_nested_arrays.js
@@ -0,0 +1,13 @@
+const printItems = function(array) {
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      printItems(item);
+    } else {
+      console.log(item);
+    }
+    
+  });
+}
+
+const array = ["😎", [["💩", ["🤗"]], [[["😼"]], "😂"]]];
+printItems(array);
