635d770754b5269fd5378820a2f064e34b69a941
bbjarvis <brettbjarvis@gmail.com>
2022-06-23 07:57:20 -0700

    add mapFnxn, mapTest

---
+++ fnxns/mapFunxn/mapTest.js
@@ -0,0 +1,3 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+const wordsToNums = lighthouses.map(x => x.length);
+console.log(wordsToNums);
