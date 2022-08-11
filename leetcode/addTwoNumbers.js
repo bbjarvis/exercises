d6de2bf6bf896431e43d19492491c149f2a10b2e
bbjarvis <brettbjarvis@gmail.com>
2022-08-11 15:35:46 -0700

    take into account edge cases

--- leetcode/addTwoNumbers.js
+++ leetcode/addTwoNumbers.js
@@ -22,12 +22,11 @@ Constraints:
 The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.
-
 /**
  * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ * FUNCTION LISTNODE(VAL, NEXT) {
+ *     THIS.VAL = (VAL===UNDEFINED ? 0 : VAL)
+ *     THIS.NEXT = (NEXT===UNDEFINED ? NULL : NEXT)
  * }
  */
 /**
@@ -35,44 +34,39 @@ It is guaranteed that the list represents a number that does not have leading ze
  * @param {ListNode} l2
  * @return {ListNode}
  */
- function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
-}
-var addTwoNumbers = function(l1, l2) {
-    const l3 = new ListNode();
-    const currl3 = l3;
-    const currl1 = l1;
-    const currl2 = l2;
 
-    while (currl1.val !== null || currl2.val !== null ) {
-        
-        if (!currl3.val) {currl3.val = 0};
-        if (!currl1.val) {currl1.val = 0};
-        if (!currl2.val) {currl2.val = 0};
+ var addTwoNumbers = function(l1, l2) {
+    let l3 = new ListNode();
+    let currL3 = l3;
+    let head1 = l1;
+    let head2 = l2;
+    let add1 = 0;
+    let currHead1 = 0;
+    let currHead2 = 0;
+    let count = 0;
+    
 
-        currl3.val = currl1.val + currl2.val + currl3.val;
-    console.log(currl3)
-        
-        if (currl3.val > 9) {
-            currl3.val = currl3.val - 10;
-            currl3.next = new ListNode()
-            currl3.next = 1}
-    console.log(currl3)
-        currl1.val = currl1.next
-        currl2.val = currl2.next
-        currl3.next = new ListNode();
-        currl3 = currl3.next;
+    while (head1 !== null || head2 !== null) {
+        if (count !== 0){
+        currL3.next = new ListNode()
+        currL3 = currL3.next}         
 
+        head1 !== null ? currHead1 = head1.val : currHead1 = 0
+        head2 !== null ? currHead2 = head2.val : currHead2 = 0       
+
+        currL3.val = currHead1 + currHead2 + add1;     
+        add1 = 0;
+        if (head1 !== null) {head1 = head1.next;};
+        if (head2 !== null) {head2 = head2.next;};
+        if (currL3.val > 9) {
+            currL3.val = currL3.val - 10;            
+            add1 = 1}
+        count++
+        if (head1 === null && head2 === null && add1 === 1) {
+            currL3.next = new ListNode()
+            currL3 = currL3.next
+            currL3.val = add1;
+        }
     }
-    return l3;
-};
-const l1 = 
-[2,4,3]
-// [0]
-// [9,9,9,9,9,9,9]
-const l2 = 
-[5,6,4]
-// [0]
-// [9,9,9,9]
-console.log(addTwoNumbers(l1, l2))
+    return l3
+    }
