8b2574bfa0d90be96908227a4aa5fe5cc3fa01fe
Brett Jarvis <brettbjarvis@gmail.com>
2024-02-08 15:19:19 -0800

    first try at output, undefined

--- leetcode/longestSub.js
+++ leetcode/longestSub.js
@@ -28,4 +28,13 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {}
+var lengthOfLongestSubstring = function (s) {
+  let count = 0
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < s.length; j++) {
+      if (s[i] === s[j]) {
+        count++
+      }
+    }
+  }
+}
